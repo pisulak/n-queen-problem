7861f65b15e57f843e25f09fc06a5c0f39ae249a
Kacper Pisula <127031073+pisulak@users.noreply.github.com>
2024-06-21 21:11:40 +0200

    prettier code, more universal approach

--- main.cpp
+++ main.cpp
@@ -3,156 +3,150 @@
 
 using namespace std;
 
-struct Tile{
-    bool occupied=false;
-    bool threatened=false;
+struct Tile {
+    bool occupied = false;
+    bool threatened = false;
 };
 
 int letterInterpreter(char letter) {
-    switch (letter) {
-        case 'a': return 0;
-        case 'b': return 1;
-        case 'c': return 2;
-        case 'd': return 3;
-        case 'e': return 4;
-        case 'f': return 5;
-        case 'g': return 6;
-        case 'h': return 7;
-        case 'i': return 8;
-        case 'j': return 9;
-        case 'k': return 10;
-        case 'l': return 11;
-        case 'm': return 12;
-        case 'n': return 13;
-        case 'o': return 14;
-        case 'p': return 15;
-        case 'r': return 16;
-        case 's': return 17;
-        case 't': return 18;
-        case 'u': return 19;
-        case 'w': return 20;
-        case 'x': return 21;
-        case 'y': return 22;
-        case 'z': return 23;
-        default: return -1;
+    if (letter >= 'A' && letter <= 'Z') {
+        return letter - 'A';
+    } else if (letter >= 'a' && letter <= 'z') {
+        return letter - 'a';
+    } else {
+        return -1;
     }
 }
 
 void checkForThreat(vector<vector<Tile> >& chessboard, int row, int col, const int& size, bool thread) {
-    for(int i=0; i<size; ++i){
-        chessboard[row][i].threatened=thread;
+    for (int i = 0; i < size; ++i) {
+        chessboard[row][i].threatened = thread;
+        chessboard[i][col].threatened = thread;
     }
-    for(int i=0; i<size; ++i){
-        chessboard[i][col].threatened=thread;
+
+    // Upper left diagonal
+    for (int i = row, j = col; i >= 0 && j >= 0; --i, --j) {
+        chessboard[i][j].threatened = thread;
     }
-    
-    // lewa gorna
-    for (int i=row, j=col; i>=0 && j>=0; --i, --j) {
-        chessboard[i][j].threatened=thread;
+    // Upper right diagonal
+    for (int i = row, j = col; i >= 0 && j < size; --i, ++j) {
+        chessboard[i][j].threatened = thread;
     }
-    // prawa gorna
-    for (int i=row, j=col; i>=0 && j<size; --i, ++j) {
-        chessboard[i][j].threatened=thread;
+    // Bottom left diagonal
+    for (int i = row, j = col; i < size && j >= 0; ++i, --j) {
+        chessboard[i][j].threatened = thread;
     }
-    // lewa dolna
-    for (int i=row, j=col; i<size && j>=0; ++i, --j) {
-        chessboard[i][j].threatened=thread;
-    }
-    // prawa dolna
-    for (int i=row, j=col; i<size && j<size; ++i, ++j) {
-        chessboard[i][j].threatened=thread;
+    // Bottom right diagonal
+    for (int i = row, j = col; i < size && j < size; ++i, ++j) {
+        chessboard[i][j].threatened = thread;
     }
 }
 
-void showChessboardWithThreat(vector<vector<Tile> >& chessboard, const int& size){
-    for(int i=0; i<size; ++i){
-        for(int j=0; j<size; ++j){
-            if(chessboard[i][j].occupied)
-                cout<<"[H]  ";
-            else {
-                if(chessboard[i][j].threatened)
-                    cout<<"[T]  ";
-                else
-                    cout<<"[_]  ";
+void showChessboardWithThreat(vector<vector<Tile> >& chessboard, const int& size) {
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            if (chessboard[i][j].occupied) {
+                cout << "[Q]  ";
+            } else {
+                if (chessboard[i][j].threatened) {
+                    cout << "[T]  ";
+                } else {
+                    cout << "[_]  ";
+                }
             }
         }
-        cout<<endl<<endl;
+        cout << endl << endl;
     }
 }
 
-void showChessboard(vector<vector<Tile> >& chessboard, const int& size){
-    for(int i=0; i<size; ++i){
-        for(int j=0; j<size; ++j){
-            if(chessboard[i][j].occupied)
-                cout<<"[H]  ";
-            else 
-               cout<<"[_]  ";
+void showChessboard(vector<vector<Tile> >& chessboard, const int& size) {
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            if (chessboard[i][j].occupied) {
+                cout << "[Q]  ";
+            } else {
+                cout << "[_]  ";
+            }
         }
-        cout<<endl<<endl;
+        cout << endl << endl;
     }
 }
 
 void solveBoard(vector<vector<Tile> >& chessboard, int row, int startingRow, int startingCol, const int& size, int& solutions) {
-    if (row==size){
+    if (row == size) {
         solutions++;
-        cout<<"Solution "<<solutions<<":"<<endl;
+        cout << "Solution " << solutions << ":" << endl;
         showChessboard(chessboard, size);
-        cout<<endl;
+        cout << endl;
         return;
     }
 
-    if (row==startingRow) {
-        solveBoard(chessboard, row+1, startingRow, startingCol, size, solutions);
+    if (row == startingRow) {
+        solveBoard(chessboard, row + 1, startingRow, startingCol, size, solutions);
         return;
     }
 
-    for (int col=0; col<size; ++col) {
+    for (int col = 0; col < size; ++col) {
         if (!chessboard[row][col].threatened) {
-            chessboard[row][col].occupied=true;
+            chessboard[row][col].occupied = true;
             checkForThreat(chessboard, row, col, size, true);
 
-            solveBoard(chessboard, row+1, startingRow, startingCol, size, solutions);
+            solveBoard(chessboard, row + 1, startingRow, startingCol, size, solutions);
 
-            chessboard[row][col].occupied=false;
+            chessboard[row][col].occupied = false;
             checkForThreat(chessboard, row, col, size, false);
 
             checkForThreat(chessboard, startingRow, startingCol, size, true);
-            for (int i=0; i<size; ++i) {
-                for (int j=0; j<size; ++j) {
-                    if (chessboard[i][j].occupied)
+            for (int i = 0; i < size; ++i) {
+                for (int j = 0; j < size; ++j) {
+                    if (chessboard[i][j].occupied) {
                         checkForThreat(chessboard, i, j, size, true);
+                    }
                 }
             }
         }
     }
 }
 
-
 int main() {
-    const int size=8;
+    int size;
+    cout << "Enter chessboard size (max 26): ";
+    cin >> size;
+
     vector<vector<Tile> > chessboard(size, vector<Tile>(size));
 
-    string choice;
-    cout<<"Podaj pole w ktorym chcesz ustawic 1 hetmana: ";
-    cin>>choice;
+    string firstQueen;
+    cout << "Enter the tile that you want to put the first queen (format [A-Za-z][1-26]): ";
+    cin >> firstQueen;
 
-    char sizeChar = '0' + size;
-    char colLetter=choice[0];
-    int col=letterInterpreter(colLetter);
-    int row=sizeChar-choice[1];
+    char colLetter = firstQueen[0];
+    int col = letterInterpreter(colLetter);
+    int row = 0;
 
-    if(row>=0 && row<size && col>=0 && col<size){
-        cout<<"Wybrales kolumne "<<choice[0]<<" oraz rzad "<<choice[1]<<", aktualny stan tablicy: "<<endl<<endl;
+    if (firstQueen.size() == 3) {
+        int firstDigit = firstQueen[1] - '0';
+        int secondDigit = firstQueen[2] - '0';
+        row = size - ((firstDigit * 10) + secondDigit);
+    } else {
+        int digit = firstQueen[1] - '0';
+        row = size - digit;
+    }
 
-        chessboard[row][col].occupied=true;
+    if (col == -1 || row < 0 || row >= size || col < 0 || col >= size) {
+        cout << "Out of bounds values.";
+    } else {
+        cout << "You put the queen in column " << firstQueen[0] << " and row " << firstQueen.substr(1) << ".\nChessboard now looks like this (Q - queen, T - threatened): " << endl;
+
+        chessboard[row][col].occupied = true;
         checkForThreat(chessboard, row, col, size, true);
         showChessboardWithThreat(chessboard, size);
 
         int solutions = 0;
         solveBoard(chessboard, 0, row, col, size, solutions);
+        if (solutions == 0) {
+            cout << "No solutions for this chessboard" << endl;
+        }
     }
-    else
-        cout<<"Sa to wartosci poza granicami szachownicy.";
 
     return 0;
 }
